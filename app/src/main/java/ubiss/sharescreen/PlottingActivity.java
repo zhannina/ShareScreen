ddf44746e1feecfc49f061ac2b2da2c70737d2cb
Daniel Buschek <daniel.buschek@ifi.lmu.de>
2015-06-10 12:49:40 +0300

    plotting panel

---
+++ app/src/main/java/ubiss/sharescreen/PlottingActivity.java
@@ -0,0 +1,48 @@
+package ubiss.sharescreen;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import ubiss.sharescreen.gui.SensorReadingsDisplay;
+
+
+public class PlottingActivity extends ActionBarActivity {
+
+
+    protected SensorReadingsDisplay sensorDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plotting);
+
+        this.sensorDisplay = new SensorReadingsDisplay(this);
+        LinearLayout container = (LinearLayout) findViewById(R.id.container_sensor_readings_display);
+        container.addView(sensorDisplay);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_plotting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
